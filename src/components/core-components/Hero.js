7bc52eb6f4bafb3c55f1e009ca5a01d7ac2551ae
BuildTools <clayloneman@gmail.com>
2021-10-03 20:45:49 -0500

    fix hero dark mode and mobile gradient

--- src/components/core-components/Hero.js
+++ src/components/core-components/Hero.js
@@ -1,14 +1,14 @@
 const Hero = ({ h1, h2, p }) => {
   return (
-    <div className="w-full sm:px-6 lg:px-8 mx-auto bg-gradient-to-r from-yellow-600 via-pink-500 to-red-500 flex justify-center">
+    <div className="w-full sm:px-6 lg:px-8 mx-auto bg-gradient-to-r from-yellow-600 sm:via-pink-500 to-red-500 flex justify-center">
       <div class="max-w-6xl py-6 text-center sm:py-36 sm:px-0">
-        <h1 class="text-4xl w-full tracking-tight font-bold text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl">
+        <h1 class="text-4xl w-full tracking-tight font-bold text-gray-50 sm:text-5xl md:text-6xl">
           <span class="block xl:inline">{h1}</span>
         </h1>
-        <h2 class="text-2xl w-full tracking-tight font-bold text-gray-900 dark:text-gray-200 sm:text-3xl md:text-4xl p-2">
+        <h2 class="text-2xl w-full tracking-tight font-bold text-gray-200 sm:text-3xl md:text-4xl p-2">
           {h2}
         </h2>
-        <p class="text-2xl w-full sm:w-full tracking-tight font-medium text-gray-500 dark:text-gray-200 sm:text-2xl md:text-3xl p-2">
+        <p class="text-2xl w-full sm:w-full tracking-tight font-mediumtext-gray-200 sm:text-2xl md:text-3xl p-2">
           {p}
         </p>
       </div>
